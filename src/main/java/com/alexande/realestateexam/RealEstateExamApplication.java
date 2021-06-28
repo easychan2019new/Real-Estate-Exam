06a6ce2ed40ee2acb225262cbc1d9446a319371b
Xinchen Yi <easychan2019@gmail.com>
2021-06-28 14:45:58 -0400

    6.28 deploy

--- src/main/java/com/alexande/realestateexam/RealEstateExamApplication.java
+++ src/main/java/com/alexande/realestateexam/RealEstateExamApplication.java
@@ -2,12 +2,18 @@ package com.alexande.realestateexam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RealEstateExamApplication {
+public class RealEstateExamApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealEstateExamApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(RealEstateExamApplication.class);
+	}
 }
